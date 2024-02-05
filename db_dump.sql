0958a51226532f03a9137e2c9b98c06a369ffe43
Carlos <carlosterrero2@gmail.com>
2024-02-05 16:27:59 +0100

    Add db_dump users table

--- db_dump.sql
+++ db_dump.sql
@@ -1,2 +1,13 @@
 CREATE DATABASE IF NOT EXISTS tournament_wizard;
 USE tournament_wizard;
+
+CREATE TABLE IF NOT EXISTS `users` (
+`id` INT AUTO_INCREMENT NOT NULL,
+`username` VARCHAR(255) NOT NULL,
+`password` VARCHAR(255) NOT NULL,
+`recovery_question` INT NOT NULL,
+`recovery_answer` VARCHAR(255) NOT NULL,
+PRIMARY KEY(`id`)
+);
+
+SELECT * FROM users;
