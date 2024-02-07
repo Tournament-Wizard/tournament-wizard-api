7be887649721f5053fa4375187c1c000414d08de
Carlos <carlosterrero2@gmail.com>
2024-02-07 16:14:49 +0100

    Add users on SQL dump

--- db_dump.sql
+++ db_dump.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS tournament_wizard;
 USE tournament_wizard;
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE IF NOT EXISTS `user` (
 `id` INT AUTO_INCREMENT NOT NULL,
 `username` VARCHAR(255) NOT NULL,
 `password` VARCHAR(255) NOT NULL,
@@ -10,4 +10,9 @@ CREATE TABLE IF NOT EXISTS `users` (
 PRIMARY KEY(`id`)
 );
 
-SELECT * FROM users;
+INSERT INTO user (id, username, password, recovery_question, recovery_answer)
+VALUES
+(null, 'Carlos', '74df4764895198bc1c2379e94ff92ae4e58c2137b8ab8f96d81d7025345ccb6e', 2, 'mi mama'),
+(null, 'Freeze', '74df4764895198bc1c2379e94ff92ae4e58c2137b8ab8f96d81d7025345ccb6e', 3, 'mi papa');
+
+SELECT * FROM user;
