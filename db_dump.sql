7517845a523c1fd4a49bfaf4079af7b23e360fcb
Carlos <carlosterrero2@gmail.com>
2024-02-08 17:27:46 +0100

    Add apiKey security system

--- db_dump.sql
+++ db_dump.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE IF NOT EXISTS tournament_wizard;
 USE tournament_wizard;
+DROP TABLE IF EXISTS user;
 
 CREATE TABLE IF NOT EXISTS `user` (
 `id` INT AUTO_INCREMENT NOT NULL,
@@ -10,10 +11,22 @@ CREATE TABLE IF NOT EXISTS `user` (
 PRIMARY KEY(`id`)
 );
 
+CREATE TABLE IF NOT EXISTS `api_keys` (
+`id` INT AUTO_INCREMENT NOT NULL,
+`value` VARCHAR(255) NOT NULL,
+PRIMARY KEY(`id`)
+);
+
 INSERT INTO user (id, username, password, recovery_question, recovery_answer)
 VALUES
 (null, 'Carlos', '74df4764895198bc1c2379e94ff92ae4e58c2137b8ab8f96d81d7025345ccb6e', 2, 'mi mama'),
 (null, 'Freeze', '74df4764895198bc1c2379e94ff92ae4e58c2137b8ab8f96d81d7025345ccb6e', 3, 'mi papa');
 
-SELECT * FROM user;
+INSERT INTO api_keys (id, value)
+VALUES
+(null, '6872eee4-6c81-4625-a7ae-9c85d14033b9'),
+(null, 'dc635b62-8f4b-4bb7-ac77-0bb3c67c4fbc'),
+(null, '78f10d8f-6048-474d-96df-16b7339650f7');
 
+SELECT * FROM user;
+SELECT * FROM api_keys;
--- src/main/java/com/tournament_wizard/TournamentWizard/config/ApiKeyConfig.java
+++ src/main/java/com/tournament_wizard/TournamentWizard/config/ApiKeyConfig.java
@@ -1,51 +1,35 @@
 package com.tournament_wizard.TournamentWizard.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @Configuration
 public class ApiKeyConfig {
-    @Value("${api.keys}")
-    private String[] apiKeys;
 
-    public enum Permission{
-        READ, WRITE, DELETE
+    private final JdbcTemplate jdbcTemplate;
+    private final List<String> apiKeys;
+    @Autowired
+    public ApiKeyConfig(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.apiKeys = loadApiKeys();
     }
-    // Map to store API keys and their permissions.
-    private Map<String, Set<Permission>> apiKeyPermissions = new HashMap<>();
 
-
-    public ApiKeyConfig(){
-        // Initialize the map with API keys and their permissions.
-        // Permission1, permission for admins.
-        Set<Permission> permissions1 = new HashSet<>();
-        permissions1.add(Permission.READ);
-        permissions1.add(Permission.WRITE);
-        permissions1.add(Permission.DELETE);
-
-        // Permission2, permission for registered people.
-        Set<Permission> permissions2 = new HashSet<>();
-        permissions2.add(Permission.READ);
-        permissions2.add(Permission.WRITE);
-
-        // Permission3, permission for unregistered people.
-        Set<Permission> permissions3 = new HashSet<>();
-        permissions3.add(Permission.READ);
-
-        apiKeyPermissions.put("key1", permissions1);
-        apiKeyPermissions.put("key2", permissions2);
-        apiKeyPermissions.put("key3", permissions3);
+    private List<String> loadApiKeys(){
+        List<String> results = new ArrayList<>();
+        String sql = "SELECT id, value FROM api_keys";
+        jdbcTemplate.query(sql, rs -> {
+            String apiKey = rs.getString("value");
+            results.add(apiKey);
+        });
+        return results;
     }
+
     public boolean isValidApiKey(String apiKey) {
-        return apiKeyPermissions.containsKey(apiKey);
+        return apiKeys.contains(apiKey);
     }
 
-    public Set<Permission> getPermissionsForApiKey(String apiKey) {
-        return apiKeyPermissions.get(apiKey);
-    }
 }
